4d140202da797df9f76dbf0092a9de21664bd3f8
Pedini Jayashree <134771316+pedinistar@users.noreply.github.com>
2025-10-11 12:59:26 +0530

    Cart Tests

---
+++ tests/cart.spec.js
@@ -0,0 +1,13 @@
+import { test, expect } from './fixtures/test.fixture.js'
+import { addProductsToCart, navigateToCart } from './helpers/cart.helper.js'
+
+// TC08
+test("should display cart with added items", async ({ authenticatedPage }) => {
+    await addProductsToCart(authenticatedPage, 2)
+
+    await expect(authenticatedPage.locator('[data-test="shopping-cart-badge"]')).toHaveText("2")
+
+    await authenticatedPage.locator('[data-test="shopping-cart-link"]').click()
+
+    await expect(authenticatedPage.locator('[data-test="inventory-item"]')).toHaveCount(2)
+})
