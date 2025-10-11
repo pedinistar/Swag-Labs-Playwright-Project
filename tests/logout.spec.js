1bb332b05bee5f094e72d6d3e41de123a53d7564
Pedini Jayashree <134771316+pedinistar@users.noreply.github.com>
2025-10-11 13:23:00 +0530

    Logout Test

---
+++ tests/logout.spec.js
@@ -0,0 +1,14 @@
+import { test, expect } from './fixtures/test.fixture.js'
+import { addProductsToCart, navigateToCart } from './helpers/cart.helper.js'
+
+// TC13
+test("should logout user and redirect to login authenticatedPage", async ({ authenticatedPage }) => {
+    await addProductsToCart(authenticatedPage, 2)
+    await navigateToCart(authenticatedPage)
+
+    await authenticatedPage.getByRole("button", { name: "Open Menu" }).click()
+    await authenticatedPage.getByRole("link", { name: "Logout" }).click()
+
+    await expect(authenticatedPage.getByRole("button", { name: "Login" })).toBeVisible()
+    await expect(authenticatedPage).toHaveURL("https://www.saucedemo.com/")
+})
